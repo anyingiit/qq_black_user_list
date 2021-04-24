1f2d66b1a76a469e5187388561703993ae0d7ce9
anyingiit <49945850+anyingiit@users.noreply.github.com>
2021-04-24 13:19:36 +0800

    1. 修复移动配置文件引起的路径问题 2. 修复casbin model文件错误配置

--- pkg/casbin/initCasbin.go
+++ pkg/casbin/initCasbin.go
@@ -7,7 +7,7 @@ import (
 var Casbin *casbin.Enforcer
 
 func InitCasbin() (err error) {
-	if Casbin, err = casbin.NewEnforcer("conf/casbin_model.conf", "conf/casbin_rabc.csv"); err != nil {
+	if Casbin, err = casbin.NewEnforcer("conf/casbin/casbin_model.conf", "conf/casbin/casbin_rabc.csv"); err != nil {
 		return err
 	}
 	if err = Casbin.LoadModel(); err != nil {
